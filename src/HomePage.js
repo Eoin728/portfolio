34ea00e7b5ff80fd3c69fff2849d311069dc038e
eoin728 <eoiniecummins@gmail.com>
2024-08-19 15:50:45 +0100

    updated information

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,38 +1,26 @@
-import React from 'react'
-import img from './placeholder.png'
-import Project from './Project';
-import Stack from '@mui/material/Stack';
-import Head from './Head';
-import CV from './CV';
-import background from './backgroun2.jpg'
-import { Typography } from '@mui/material';
+import React from "react";
+import Project from "./Project";
+import Stack from "@mui/material/Stack";
+import background from "./backgroun2.jpg";
+import { Typography } from "@mui/material";
+import { info } from "./information";
 
-const HomePage = () =>{
-  const d1 = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-  const d2 = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-  const d3 = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-  const d4 = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
-  const titles = ["T1","T2","T3","T4"]
-  const descs = [d1,d2,d3,d4]
-  const images= [img,img,img,img]
-  return (
-    <div
-     style={{ backgroundImage: `url(${background})` }}
-    >
-    <Head/>
-    <CV/>
-    <Typography variant='h2' style = {{fontFamily: 'Ga Maamli' }} >Projects</Typography>
-<Stack spacing={4}>
-      <Project image ={images[0]} text = {descs[0]} title = {titles[0]}  link = 'none'/>
-      <Project image ={images[1]} text = {descs[1]} title = {titles[1]}  link = 'none'/>
-      <Project image ={images[2]} text = {descs[2]} title = {titles[2]}  link = 'none'/>
-      <Project image ={images[3]} text = {descs[3]} title = {titles[3]}  link = 'none'/>
-</Stack>
-     </div>
-   )
- }
+const HomePage = () => {
+    return (
+        <div style={{ backgroundImage: `url(${background})` }}>
+            <Typography
+                variant="h2"
+                style={{ fontFamily: "Ga Maamli", paddingBottom: 40 }}
+            >
+                Eoin Cummins
+            </Typography>
+            <Stack spacing={4}>
+                {info.map((i) => {
+                    return <Project info={i} link="none" />;
+                })}
+            </Stack>
+        </div>
+    );
+};
 
-
-
-
-export default HomePage
+export default HomePage;
--- src/Project.js
+++ src/Project.js
@@ -1,55 +1,107 @@
-import * as React from 'react';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
 
- 
-const Project = props =>{
-  return (
-    <div>
+const Project = (props) => {
+    return (
+        <div>
+            <Stack direction="row" justifyContent="center">
+                <Paper
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        width: "40%",
+                        borderRadius: 25,
+                        backgroundColor: "#987654",
+                    }}
+                    square={false}
+                    elevation={32}
+                >
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant="h3"
+                            style={{ fontFamily: "Ga Maamli" }}
+                            component="div"
+                        >
+                            {props.info.title}
+                        </Typography>
+                        <Typography
+                            style={{
+                                flex: 1,
+                                fontFamily: "Kode Mono",
+                                fontStyle: "italic",
+                            }}
+                            variant="body1"
+                            color="text.secondary"
+                        >
+                            {props.info.description}
+                        </Typography>
+                        <ul
+                            style={{
+                                listStyleType: "circle",
+                                // listStylePosition: "inside",
+                                paddingLeft: 20,
+                                textAlign: "start",
+                                margin: 10,
+                            }}
+                        >
+                            {props.info.bulletpoints.map((i) => {
+                                return (
+                                    <li style={{ paddingTop: 15 }}>
+                                        <Typography
+                                            style={{
+                                                flex: 1,
+                                                fontFamily: "Kode Mono",
+                                            }}
+                                            variant="body1"
+                                            color="text.secondary"
+                                        >
+                                            {i}
+                                        </Typography>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            style={{ backgroundColor: "#B57E3F" }}
+                            size="large"
+                            variant="contained"
+                            as="a"
+                            href={props.info.codelink}
+                        >
+                            <Typography style={{ fontFamily: "Kode Mono" }}>
+                                {props.info.otherlink == null
+                                    ? "View code/download"
+                                    : "View code"}
+                            </Typography>
+                        </Button>
+                        {props.info.otherlink != null && (
+                            <Button
+                                style={{ backgroundColor: "#B57E3F" }}
+                                size="large"
+                                variant="contained"
+                                as="a"
+                                href={props.info.otherlink}
+                            >
+                                <Typography style={{ fontFamily: "Kode Mono" }}>
+                                    {props.info.otherlinkname}
+                                </Typography>
+                            </Button>
+                        )}
+                    </CardActions>
+                </Paper>
+                <img src={props.info.image} width="30%"></img>
+            </Stack>
+        </div>
+    );
+};
 
-<Stack
-  direction="row"
-  justifyContent="center"
->
-
-  <Paper style = {{display:'flex', flexDirection:'column', justifyContent:'space-between', width:'40%', borderRadius:25,  backgroundColor: '#6E260E'}} square= {false} elevation={32}>
-      <CardContent>
-        <Typography gutterBottom variant='h3' style = {{fontFamily: 'Ga Maamli' }} component="div">
-          {props.title}
-        </Typography>
-        <Typography style = {{flex: 1,fontFamily: 'Kode Mono'  }} variant="body1"color="text.secondary">
-          {props.text}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button style = {{ backgroundColor: '#005387' }}size="large" variant = 'contained'>   
-          <Typography  style = {{fontFamily: 'Kode Mono' }} >
-          view code</Typography>
-
-
-        </Button>
-        <Button  style = {{ backgroundColor: '#005387' }} size="large"   variant = 'contained'>
-          
-          <Typography  style = {{fontFamily: 'Kode Mono' }} >
-          maybe something else dont really need
-          </Typography>
-          
-          </Button>
-      </CardActions>
-    </Paper>
-      <img  src = {props.image} width = '30%' ></img>
-    </Stack>
-
-     </div>
-   )
- }
-
-
-
-
-export default Project
-
+export default Project;
